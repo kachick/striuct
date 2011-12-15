cb6b6cbce973a85d7c6ea3498faa1487cacc1ce2
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2011-12-16 00:07:21 +0900

    modify #initialize_copy() and add .initialize_copy()

--- test/test_striuct.rb
+++ test/test_striuct.rb
@@ -250,4 +250,14 @@ class TestStriuctCloning < Test::Unit::TestCase
     @sth.sth = 2
     assert_equal false, (@sth.sth == sth2.sth)
   end
+  
+  def test_class_clone
+    klass = Sth.dup
+    Sth.__send__ :member, :dummy1
+    assert_equal false, klass.member?(:dummy)
+    
+    klass = Sth.clone
+    Sth.__send__ :member, :dummy2
+    assert_equal false, klass.member?(:dummy)
+  end
 end
