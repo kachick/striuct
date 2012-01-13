31623739d88fdc2ef69987a6227454447db63387
Kenichi Kamiya <kachick1@gmail.com>
2012-01-13 23:31:33 +0900

    add trace checker, modify path of example.rb

---
+++ example/see_trace.rb
@@ -0,0 +1,28 @@
+#/usr/bin/ruby -w
+
+require_relative 'lib/striuct'
+
+class User < Striuct.new
+  member :id,   Integer
+  member :age,  (20..140)
+end
+
+user = User.new
+
+begin
+  user[:id] = 10.0
+rescue
+  puts $!.backtrace
+end
+
+begin
+  user.age = 19
+rescue
+  puts $!.backtrace
+end
+
+puts '-' * 80
+
+$stdout.flush
+
+user.age = 19
