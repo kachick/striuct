ddb10f2be1689fceef1ca603cb82cc9f7509f310
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-01-14 01:06:38 +0900

    modify unassign, assign & add clear_at, reset_at

--- test/test_striuct.rb
+++ test/test_striuct.rb
@@ -420,9 +420,17 @@ class TestStriuctAssign < Test::Unit::TestCase
     assert_equal true, sth.assign?(:foo)
     sth.unassign :foo
     assert_equal false, sth.assign?(:foo)
+    sth.foo = nil
+    assert_equal true, sth.assign?(:foo)
+    sth.clear_at 0
+    assert_equal false, sth.assign?(:foo)
     
     assert_raises NameError do
       sth.unassign :var
+    end
+    
+    assert_raises IndexError do
+      sth.unassign 1
     end
   end
 end
