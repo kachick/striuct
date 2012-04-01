e002e39ad05a9b5ff7eef4377e5c6bf9ef8938f6
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-04-02 00:42:43 +0900

    modify file tree

--- lib/striuct.rb
+++ lib/striuct.rb
@@ -1,3 +1,16 @@
 # Copyright (C) 2011  Kenichi Kamiya
+# Striuct
+#   Provides a Struct++ class.
 
-require_relative 'striuct/frame'
+# @author Kenichi Kamiya
+# @abstract
+class Striuct
+  class ConditionError < ArgumentError; end
+  
+  # namespace for .to_struct_class, #to_struct
+  module Structs
+  end
+end
+
+require_relative 'striuct/version'
+require_relative 'striuct/abstract'
