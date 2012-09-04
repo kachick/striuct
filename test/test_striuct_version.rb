917541747a09e79106479be4a80774ef67f8c38f
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-09-05 00:11:11 +0900

    add Version test-caser

--- test/test_striuct_version.rb
+++ test/test_striuct_version.rb
@@ -3,6 +3,8 @@ require_relative 'helper'
 class TestStriuctVersion < Test::Unit::TestCase
 
   def test_const_version
+    assert_kind_of String, Striuct::VERSION
+    assert(/\A\d+\.\d+\.\d+(\.\w+)?\z/ =~ Striuct::VERSION)
     assert_equal true, Striuct::VERSION.frozen?
   end
 
