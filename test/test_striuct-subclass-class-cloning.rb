66448dbc637e23d886a62a6c0944a2f8d90cbe1d
Kenichi Kamiya <kachick1@gmail.com>
2012-09-06 18:18:39 +0900

    improve tests file tree

--- test/test_striuct-subclass-class-cloning.rb
+++ test/test_striuct-subclass-class-cloning.rb
@@ -1,26 +1,11 @@
-$VERBOSE = true
 require_relative 'helper'
 
-class TestStriuctCloning < Test::Unit::TestCase
+class TestStriuctClassCloning < Test::Unit::TestCase
 
   class Sth < Striuct.new
     member :sth
   end
-  
-  def setup
-    @sth = Sth.new
-  end
-  
-  def test_clone
-    sth2 = @sth.dup
-    @sth.sth = 1
-    assert_equal false, (@sth.sth == sth2.sth)
-    
-    sth2 = @sth.clone
-    @sth.sth = 2
-    assert_equal false, (@sth.sth == sth2.sth)
-  end
-  
+
   def test_class_dup
     klass = Sth.dup
     Sth.__send__ :member, :dummy1
