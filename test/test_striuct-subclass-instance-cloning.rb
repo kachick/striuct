fe529436ead01e1bc3a55089af7a312ac9bd1142
Kenichi Kamiya <kachick1+github@gmail.com>
2012-09-06 18:18:39 +0900

    improve tests file tree

---
+++ test/test_striuct-subclass-instance-cloning.rb
@@ -0,0 +1,24 @@
+$VERBOSE = true
+require_relative 'helper'
+
+class TestStriuctInstanceCloning < Test::Unit::TestCase
+
+  class Sth < Striuct.new
+    member :sth
+  end
+  
+  def test_dup
+    sth = Sth.new
+    sth2 = sth.dup
+    sth.sth = 1
+    assert_equal false, (sth.sth == sth2.sth)
+  end
+  
+  def test_clone
+    sth = Sth.new
+    sth2 = sth.clone
+    sth.sth = 2
+    assert_equal false, (sth.sth == sth2.sth)
+  end
+
+end
