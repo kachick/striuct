c22bddb2171ece9af52278f006e45387d8b6f83f
Kenichi Kamiya <kachick1@gmail.com>
2012-09-06 21:38:09 +0900

    Modify for get compatibility under Ruby2.0~ behavior. Ruby2.0 modified Kernel#__callee_

--- lib/striuct/instancemethods/keyvalidatable.rb
+++ lib/striuct/instancemethods/keyvalidatable.rb
@@ -2,11 +2,12 @@ require 'keyvalidatable'
 
 class Striuct; module InstanceMethods
 
+  extend Forwardable
   include KeyValidatable
 
   KeyValidatable.instance_methods.each do |feature|
     if specific_feature = feature.to_s.sub!('members'){'autonyms'}
-      alias_method specific_feature.to_sym, feature
+      def_delegator :self, feature, specific_feature.to_sym
     end
   end
 
