8e92ba156f2c41216237ce5862ed4b94e2d97d29
Kenichi Kamiya <kachick1@gmail.com>
2012-09-07 03:36:38 +0900

    improve Class" s singleton_class methods style

--- lib/striuct/singleton_class.rb
+++ lib/striuct/singleton_class.rb
@@ -1,7 +1,7 @@
 require_relative 'instancemethods'
 
 class Striuct
-
+  
   class << self
 
     # @group Constructor
@@ -45,18 +45,8 @@ class Striuct
     private
     
     alias_method :original_inherited, :inherited
-    alias_method :original_initialize_copy, :initialize_copy
 
     def inherited(subclass)
-      if equal? ::Striuct
-        head_inherited subclass
-        subclass.singleton_class.__send__ :undef_method, :head_inherited
-      else
-        tail_inherited subclass
-      end
-    end
-
-    def head_inherited(subclass)
       subclass.class_eval do
         original_inherited subclass
 
@@ -68,42 +58,7 @@ class Striuct
         @conditions, @adjusters, @defaults, @inferences, @aliases, @setter_validations, @getter_validations = \
           {}, {}, {}, {}, {}, {}, {}
         @protect_level = :prevent
-
-        singleton_class.__send__ :define_initialize_copy
       end
-    end
-    
-    def tail_inherited(subclass)
-      autonyms = @autonyms.dup
-      attributes = [@conditions, @adjusters, @defaults, @inferences, @aliases, @setter_validations, @getter_validations].map(&:dup)
-      protect_level = @protect_level
-      
-      subclass.class_eval do
-        original_inherited subclass
-        
-        @autonyms = autonyms
-        @conditions, @adjusters, @defaults, @inferences, @aliases, @setter_validations, @getter_validations = *attributes
-        @protect_level = protect_level
-        
-        singleton_class.__send__ :define_initialize_copy
-      end
-    end
-
-    # For Singleton Class's macro
-    class << self
-      
-      private
-    
-      def define_initialize_copy
-        define_method :initialize_copy do |original|
-          ret = original_initialize_copy original
-          @autonyms = @autonyms.dup
-          @conditions, @adjusters, @defaults, @inferences, @aliases, @setter_validations, @getter_validations = \
-          [@conditions, @adjusters, @defaults, @inferences, @aliases, @setter_validations, @getter_validations].map(&:dup)
-          ret
-        end
-      end
-    
     end
 
   end
