7ad95419113003de9fe35cceb7da39fe7be2199b
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-09-07 03:36:38 +0900

    improve Class" s singleton_class methods style

--- lib/striuct/classmethods/object.rb
+++ lib/striuct/classmethods/object.rb
@@ -20,6 +20,29 @@ class Striuct; module ClassMethods
   
   private
   
+  def inherited(subclass)
+    autonyms = @autonyms.dup
+    attributes = [@conditions, @adjusters, @defaults, @inferences, @aliases, @setter_validations, @getter_validations].map(&:dup)
+    protect_level = @protect_level
+    
+    subclass.class_eval do
+      original_inherited subclass
+      
+      @autonyms = autonyms
+      @conditions, @adjusters, @defaults, @inferences, @aliases, @setter_validations, @getter_validations = *attributes
+      @protect_level = protect_level
+    end
+  end
+     
+  def initialize_copy(original)
+    ret = super original
+    @autonyms, @adjusters, @defaults, @aliases,
+    @setter_validations, @getter_validations,
+    @conditions, @inferences    = 
+    *__floating_attributes__.map(&:dup)
+    ret
+  end
+
   def __floating_attributes__
     [@autonyms, @adjusters, @defaults, @aliases,
      @setter_validations, @getter_validations,
