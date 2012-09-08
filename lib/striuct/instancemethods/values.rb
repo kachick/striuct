aaa67e16c1f3663332a1a73b6df53bfa8e012e3e
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-09-09 02:38:19 +0900

    fix #replace_values for out of init in evaluate default value

--- lib/striuct/instancemethods/values.rb
+++ lib/striuct/instancemethods/values.rb
@@ -31,22 +31,6 @@ class Striuct; module InstanceMethods
     values.each_with_index do |value, index|
       self[index] = value
     end
-      
-    excess = _autonyms.last(size - values.size)
-      
-    excess.each do |autonym|
-      if has_default? autonym
-        default = default_value_for autonym
-        self[autonym] = (
-          if default_type_for(autonym) == :lazy
-            args = [self, autonym][0, default.arity]
-            default.call(*args)
-          else
-            default
-          end
-        )
-      end
-    end
     
     self
   end
