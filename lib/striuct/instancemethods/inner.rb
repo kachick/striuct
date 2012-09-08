4123b44174fb8d969ed592fd87a7301fe6cc9bbb
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-09-09 05:46:13 +0900

    refactoring

--- lib/striuct/instancemethods/inner.rb
+++ lib/striuct/instancemethods/inner.rb
@@ -47,6 +47,30 @@ class Striuct; module InstanceMethods
   
     raise
   end
+
+  # @param [Symbol, String, Fixnum] key
+  # @return [Symbol] autonym
+  def _autonym_for_key(key)
+    case key
+    when Symbol, String
+      name = keyable_for key
+      if member? name
+        return autonym_for(name)
+      else
+        raise NameError
+      end
+    when Fixnum
+      if autonym = _autonyms[key]
+        return autonym
+      else
+        raise IndexError
+      end
+    else
+      raise ArgumentError
+    end
+
+    raise 'must not happen'
+  end
   
   # @endgroup
 
