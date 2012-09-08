cfe10392fa8db7a056caa811f91fe90566dddc50
Kenichi Kamiya <kachick1@gmail.com>
2012-09-09 04:56:29 +0900

    refactoring

--- lib/striuct/classmethods/inner.rb
+++ lib/striuct/classmethods/inner.rb
@@ -26,35 +26,17 @@ class Striuct; module ClassMethods
 
   def __setter__!(autonym, condition, &adjuster)
     unless Validation::Condition::ANYTHING.equal? condition
-      __set_condition__! autonym, condition 
+      attributes_for(autonym).condition = condition
     end
 
     if block_given?
-      __set_adjuster__! autonym, &adjuster
+      attributes_for(autonym).adjuster = adjuster
     end
 
     define_method :"#{autonym}=" do |value|
       __set__ autonym, value
     end
  
-    nil
-  end
-  
-  def __set_condition__!(autonym, condition)
-    unless ::Validation.conditionable? condition
-      raise TypeError, 'wrong object for condition' 
-    end
-
-    attributes_for(autonym).condition = condition 
-    nil
-  end
-
-  def __set_adjuster__!(autonym, &adjuster)
-    unless ::Validation.adjustable? adjuster
-      raise ArgumentError, "wrong number of block argument #{arity} for 1"
-    end
-    
-    attributes_for(autonym).adjuster = adjuster 
     nil
   end
 
@@ -65,7 +47,7 @@ class Striuct; module ClassMethods
                                     autonym?(autonym) and
                                    _caller.instance_of?(self)
 
-    __set_condition__! autonym ,family
+    attributes_for(autonym).condition = family
     attributes_for(autonym).inference = false
 
     nil
