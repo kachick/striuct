ae3fcebb66f87260d6ae9095be4ceaf129c2d838
Kenichi Kamiya <kachick1@gmail.com>
2012-09-17 19:39:14 +0900

    key always mean name + index, basic predicate always return boolean , remove .#_nameable

--- lib/striuct/classmethods/named.rb
+++ lib/striuct/classmethods/named.rb
@@ -14,6 +14,23 @@ class Striuct; module ClassMethods
     @autonyms + @aliases.keys
   end
 
+  def has_autonym?(name)
+    @autonyms.include? name.to_sym
+  rescue NoMethodError
+    false
+  end
+  
+  alias_method :autonym?, :has_autonym?
+ 
+  def has_alias?(name)
+    @aliases.has_key? name.to_sym
+  rescue NoMethodError
+    false
+  end
+  
+  alias_method :alias?, :has_alias?
+  alias_method :aliased?, :has_alias? # obsolute
+
   def has_member?(name)
     autonym_for_name name
   rescue Exception
@@ -24,10 +41,26 @@ class Striuct; module ClassMethods
   
   alias_method :member?, :has_member?
 
+  def has_index?(index)
+    @autonyms.fetch index
+  rescue Exception
+    false
+  else
+    true
+  end
+
+  alias_method :index?, :has_index?
+
+  def has_key?(key)
+    has_member?(key) || has_index?(key)
+  end
+
+  alias_method :key?, :has_key?
+
   # @param [Symbol, String, #to_sym] name - autonym / aliased
   # @return [Symbol]
   def autonym_for_name(name)
-    name = _nameable_for name
+    name = name.to_sym
     
     return name if @autonyms.include? name
     
@@ -45,7 +78,7 @@ class Striuct; module ClassMethods
   def autonym_for_key(key)
     case key
     when Symbol, String
-      name = _nameable_for key
+      name = key.to_sym
       if member? name
         return autonym_for_name(name)
       else
@@ -63,39 +96,17 @@ class Striuct; module ClassMethods
 
     raise 'must not happen'
   end
-
-  # @param [Symbol, String] name
-  def has_autonym?(name)
-    name = _nameable_for name
-    raise NameError unless member? name
-
-    @autonyms.include? name
+  
+  def has_aliases_for?(autonym)
+    @aliases.has_value? autonym.to_sym
+  rescue NoMethodError
+    false
   end
-  
-  alias_method :autonym?, :has_autonym?
-  
-  # @param [Symbol, String] name
-  def has_alias?(name)
-    name = _nameable_for name
-    raise NameError unless member? name
 
-    @aliases.has_key? name
-  end
-  
-  alias_method :alias?, :has_alias?
-  alias_method :aliased?, :has_alias? # obsolute
-  
-  # @param [Symbol, String] autonym
-  def has_aliases?(autonym)
-    raise NameError unless autonym? autonym
-
-    @aliases.has_value? autonym
-  end
-  
   # @param [Symbol, String] autonym
   # @return [Array<Symbol>]
   def aliases_for(autonym)
-    autonym = _nameable_for autonym
+    autonym = autonym.to_sym
     raise NameError unless has_aliases? autonym
 
     @aliases.group_by{|aliased, an|an}.fetch(autonym)
@@ -108,12 +119,6 @@ class Striuct; module ClassMethods
   
   private
 
-  # @param [Symbol, String, #to_sym] name
-  # @return [Symbol]
-  def _nameable_for(name)
-    name.to_sym
-  end
-  
   # for access from own instance
   def _autonyms
     @autonyms
