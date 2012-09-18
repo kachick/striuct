7149506463d08450188a45961fce54e46d387750
Kenichi Kamiya <kachick1@gmail.com>
2012-09-18 22:05:45 +0900

    modify test-case (#54)

--- test/test_subc-i-assign.rb
+++ test/test_subc-i-assign.rb
@@ -6,24 +6,26 @@ class Test_Striuct_Subclass_Instance_Assign < Test::Unit::TestCase
     member :foo
   end  
 
-  def test_unassign
-    sth = Sth.new
-    assert_equal false, sth.assign?(:foo)
-    sth.foo = nil
-    assert_equal true, sth.assign?(:foo)
-    sth.unassign :foo
-    assert_equal false, sth.assign?(:foo)
-    sth.foo = nil
-    assert_equal true, sth.assign?(:foo)
-    sth.clear_at 0
-    assert_equal false, sth.assign?(:foo)
-    
-    assert_raises NameError do
-      sth.unassign :var
-    end
-    
-    assert_raises IndexError do
-      sth.unassign 1
+  [:unassign, :delete_at].each do |method|
+    define_method :"test_#{method}" do
+      sth = Sth.new
+      assert_equal false, sth.assign?(:foo)
+      sth.foo = nil
+      assert_equal true, sth.assign?(:foo)
+      sth.public_send method, :foo
+      assert_equal false, sth.assign?(:foo)
+      sth.foo = nil
+      assert_equal true, sth.assign?(:foo)
+      sth.public_send method, 0
+      assert_equal false, sth.assign?(:foo)
+      
+      assert_raises NameError do
+        sth.public_send method,  :var
+      end
+      
+      assert_raises IndexError do
+        sth.public_send method,  1
+      end
     end
   end
 
