d4d07d84d5c29e5d11c0eed19806555f62094ca5
Kenichi Kamiya <kachick1+github@gmail.com>
2012-09-18 13:31:25 +0900

    add test-case .#with_default?

--- test/test_subc-f-predicate.rb
+++ test/test_subc-f-predicate.rb
@@ -1,6 +1,6 @@
 require_relative 'helper'
 
-class Test_Striuct_Subclass_Predicate < Test::Unit::TestCase
+class Test_Striuct_Subclass_BasicPredicate < Test::Unit::TestCase
 
   class Subclass < Striuct
     member :foo    
@@ -134,6 +134,61 @@ class Test_Striuct_Subclass_Predicate < Test::Unit::TestCase
         assert_same true, reciever.public_send(predicate, 'bar')
         assert_same false, reciever.public_send(predicate, 'aliased')
         assert_same false, reciever.public_send(predicate, 'xyz')
+      end
+    end
+  end
+
+end
+
+
+class Test_Striuct_Subclass_Predicate_Default < Test::Unit::TestCase
+
+  class Subclass < Striuct
+    member :foo    
+    member :val, ANYTHING?, default: 'DEFAULT'
+    member :lazy, ANYTHING?, default_proc: ->{} 
+    alias_member :als_foo, :foo
+    alias_member :als_val, :val
+    alias_member :als_lazy, :lazy
+    close_member
+  end.freeze
+
+  INSTANCE = Subclass.new.freeze
+  
+  TYPE_PAIRS = {
+    class: Subclass,
+    instance: INSTANCE
+  }.freeze
+
+  [:with_default?].each do |predicate|
+    TYPE_PAIRS.each_pair do |type, reciever|
+      define_method :"test_#{type}_#{predicate}" do
+        assert_same false, reciever.public_send(predicate, :foo)
+        assert_same false, reciever.public_send(predicate, :als_foo)
+        assert_same false, reciever.public_send(predicate, 'foo')
+        assert_same false, reciever.public_send(predicate, 'als_foo')
+        assert_same false, reciever.public_send(predicate, 0)
+        assert_same false, reciever.public_send(predicate, 0.9)
+        
+        assert_same true, reciever.public_send(predicate, :val)
+        assert_same true, reciever.public_send(predicate, :als_val)
+        assert_same true, reciever.public_send(predicate, 'val')
+        assert_same true, reciever.public_send(predicate, 'als_val')
+        assert_same true, reciever.public_send(predicate, 1)
+        assert_same true, reciever.public_send(predicate, 1.9)
+        
+        assert_same true, reciever.public_send(predicate, :lazy)
+        assert_same true, reciever.public_send(predicate, :als_lazy)
+        assert_same true, reciever.public_send(predicate, 'lazy')
+        assert_same true, reciever.public_send(predicate, 'als_lazy')
+        assert_same true, reciever.public_send(predicate, 2)
+        assert_same true, reciever.public_send(predicate, 2.9)
+        
+        assert_same false, reciever.public_send(predicate, :none)
+        assert_same false, reciever.public_send(predicate, 'none')
+        assert_same false, reciever.public_send(predicate, 3)
+        assert_same false, reciever.public_send(predicate, 3.9)
+        assert_same false, reciever.public_send(predicate, BasicObject.new)
       end
     end
   end
