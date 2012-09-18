e9eb0cc62d64b919a724eb333426b5ad326b0ca6
Kenichi Kamiya <kachick1@gmail.com>
2012-09-18 12:00:02 +0900

    draft improve test-case

--- test/test_subc-f-predicate.rb
+++ test/test_subc-f-predicate.rb
@@ -2,197 +2,164 @@ require_relative 'helper'
 
 class Test_Striuct_Subclass_Predicate < Test::Unit::TestCase
 
-  class FooBar < Striuct
+  class Subclass < Striuct
     member :foo    
     member :bar
     alias_member :aliased, :bar
     close_member
+  end.freeze
+
+  INSTANCE = Subclass.new.freeze
+  
+  TYPE_PAIRS = {
+    class: Subclass,
+    instance: INSTANCE
+  }.freeze
+
+  [:autonym?, :has_autonym?].each do |predicate|
+    TYPE_PAIRS.each_pair do |type, reciever|
+      define_method :"test_#{type}_#{predicate}" do
+        assert_same true, reciever.public_send(predicate, :foo)
+        assert_same true, reciever.public_send(predicate, :bar)
+        assert_same false, reciever.public_send(predicate, :aliased)
+        assert_same false, reciever.public_send(predicate, :xyz)
+        assert_same false, reciever.public_send(predicate, 1)
+        assert_same false, reciever.public_send(predicate, BasicObject.new)
+        assert_same true, reciever.public_send(predicate, 'foo')
+        assert_same true, reciever.public_send(predicate, 'bar')
+        assert_same false, reciever.public_send(predicate, 'aliased')
+        assert_same false, reciever.public_send(predicate, 'xyz')
+      end
+    end
   end
 
-  INS = FooBar.new
-
-  def test_has_autonym?
-    assert_same true, FooBar.has_autonym?(:foo)
-    assert_same true, FooBar.has_autonym?(:bar)
-    assert_same false, FooBar.has_autonym?(:aliased)
-    assert_same false, FooBar.has_autonym?(:xyz)
-    assert_same false, FooBar.has_autonym?(1)
-    assert_same false, FooBar.has_autonym?(BasicObject.new)
-    assert_same true, FooBar.has_autonym?('foo')
-    assert_same true, FooBar.has_autonym?('bar')
-    assert_same false, FooBar.has_autonym?('aliased')
-    assert_same false, FooBar.has_autonym?('xyz')
-
-    assert_same true, INS.has_autonym?(:foo)
-    assert_same true, INS.has_autonym?(:bar)
-    assert_same false, INS.has_autonym?(:aliased)
-    assert_same false, INS.has_autonym?(:xyz)
-    assert_same false, INS.has_autonym?(1)
-    assert_same false, INS.has_autonym?(BasicObject.new)
-    assert_same true, INS.has_autonym?('foo')
-    assert_same true, INS.has_autonym?('bar')
-    assert_same false, INS.has_autonym?('aliased')
-    assert_same false, INS.has_autonym?('xyz')
+  [:alias?, :has_alias?].each do |predicate|
+    TYPE_PAIRS.each_pair do |type, reciever|
+      define_method :"test_#{type}_#{predicate}" do
+        assert_same false, reciever.public_send(predicate, :foo)
+        assert_same false, reciever.public_send(predicate, :bar)
+        assert_same true, reciever.public_send(predicate, :aliased)
+        assert_same false, reciever.public_send(predicate, :xyz)
+        assert_same false, reciever.public_send(predicate, 1)
+        assert_same false, reciever.public_send(predicate, BasicObject.new)
+        assert_same false, reciever.public_send(predicate, 'foo')
+        assert_same false, reciever.public_send(predicate, 'bar')
+        assert_same true, reciever.public_send(predicate, 'aliased')
+        assert_same false, reciever.public_send(predicate, 'xyz')
+      end
+    end
   end
 
-  def test_has_alias?
-    assert_same false, FooBar.has_alias?(:foo)
-    assert_same false, FooBar.has_alias?(:bar)
-    assert_same true, FooBar.has_alias?(:aliased)
-    assert_same false, FooBar.has_alias?(:xyz)
-    assert_same false, FooBar.has_alias?(1)
-    assert_same false, FooBar.has_alias?(BasicObject.new)
-    assert_same false, FooBar.has_alias?('foo')
-    assert_same false, FooBar.has_alias?('bar')
-    assert_same true, FooBar.has_alias?('aliased')
-    assert_same false, FooBar.has_alias?('xyz')
-
-    assert_same false, INS.has_alias?(:foo)
-    assert_same false, INS.has_alias?(:bar)
-    assert_same true, INS.has_alias?(:aliased)
-    assert_same false, INS.has_alias?(:xyz)
-    assert_same false, INS.has_alias?(1)
-    assert_same false, INS.has_alias?(BasicObject.new)
-    assert_same false, INS.has_alias?('foo')
-    assert_same false, INS.has_alias?('bar')
-    assert_same true, INS.has_alias?('aliased')
-    assert_same false, INS.has_alias?('xyz')
+  [:member?, :has_member?].each do |predicate|
+    TYPE_PAIRS.each_pair do |type, reciever|
+      define_method :"test_#{type}_#{predicate}" do
+        assert_same true, reciever.public_send(predicate, :foo)
+        assert_same true, reciever.public_send(predicate, :bar)
+        assert_same true, reciever.public_send(predicate, :aliased)
+        assert_same false, reciever.public_send(predicate, :xyz)
+        assert_same false, reciever.public_send(predicate, 1)
+        assert_same false, reciever.public_send(predicate, BasicObject.new)
+        assert_same true, reciever.public_send(predicate, 'foo')
+        assert_same true, reciever.public_send(predicate, 'bar')
+        assert_same true, reciever.public_send(predicate, 'aliased')
+        assert_same false, reciever.public_send(predicate, 'xyz')
+      end
+    end
   end
 
-  def test_has_member?
-    assert_same true, FooBar.has_member?(:foo)
-    assert_same true, FooBar.has_member?(:bar)
-    assert_same true, FooBar.has_member?(:aliased)
-    assert_same false, FooBar.has_member?(:xyz)
-    assert_same false, FooBar.has_member?(1)
-    assert_same false, FooBar.has_member?(BasicObject.new)
-    assert_same true, FooBar.has_member?('foo')
-    assert_same true, FooBar.has_member?('bar')
-    assert_same true, FooBar.has_member?('aliased')
-    assert_same false, FooBar.has_member?('xyz')
-    
-    assert_same true, INS.has_member?(:foo)
-    assert_same true, INS.has_member?(:bar)
-    assert_same true, INS.has_member?(:aliased)
-    assert_same false, INS.has_member?(:xyz)
-    assert_same false, INS.has_member?(1)
-    assert_same false, INS.has_member?(BasicObject.new)
-    assert_same true, INS.has_member?('foo')
-    assert_same true, INS.has_member?('bar')
-    assert_same true, INS.has_member?('aliased')
-    assert_same false, INS.has_member?('xyz')
-  end
-
-  def test_has_index?
-    assert_same true, FooBar.has_index?(0)
-    assert_same true, FooBar.has_index?(1)
-    assert_same false, FooBar.has_index?(2)
-    assert_same false, FooBar.has_index?(99 * 99)
-    assert_same true, FooBar.has_index?(-1)
-    assert_same true, FooBar.has_index?(-2)
-    assert_same false, FooBar.has_index?(-3)
-    assert_same false, FooBar.has_index?(-(99 * 99))
-    assert_same true, FooBar.has_index?(0.1)
-    assert_same true, FooBar.has_index?(1.9)
-    assert_same false, FooBar.has_index?('1')
-    assert_same false, FooBar.has_index?(:foo)
-    assert_same false, FooBar.has_index?(:bar)
-    assert_same false, FooBar.has_index?(:aliased)
-    assert_same false, FooBar.has_index?(:xyz)
-    assert_same false, FooBar.has_index?(BasicObject.new)
-    assert_same false, FooBar.has_index?('foo')
-    assert_same false, FooBar.has_index?('bar')
-    assert_same false, FooBar.has_index?('aliased')
-    assert_same false, FooBar.has_index?('xyz')
-    
-    assert_same true, INS.has_index?(0)
-    assert_same true, INS.has_index?(1)
-    assert_same false, INS.has_index?(2)
-    assert_same false, INS.has_index?(99 * 99)
-    assert_same true, INS.has_index?(-1)
-    assert_same true, INS.has_index?(-2)
-    assert_same false, INS.has_index?(-3)
-    assert_same false, INS.has_index?(-(99 * 99))
-    assert_same true, INS.has_index?(0.1)
-    assert_same true, INS.has_index?(1.9)
-    assert_same false, INS.has_index?('1')
-    assert_same false, INS.has_index?(:foo)
-    assert_same false, INS.has_index?(:bar)
-    assert_same false, INS.has_index?(:aliased)
-    assert_same false, INS.has_index?(:xyz)
-    assert_same false, INS.has_index?(BasicObject.new)
-    assert_same false, INS.has_index?('foo')
-    assert_same false, INS.has_index?('bar')
-    assert_same false, INS.has_index?('aliased')
-    assert_same false, INS.has_index?('xyz')
+  [:index?, :has_index?].each do |predicate|
+    TYPE_PAIRS.each_pair do |type, reciever|
+      define_method :"test_#{type}_#{predicate}" do
+        assert_same true, reciever.public_send(predicate, 0)
+        assert_same true, reciever.public_send(predicate, 1)
+        assert_same false, reciever.public_send(predicate, 2)
+        assert_same false, reciever.public_send(predicate, 99 * 99)
+        assert_same true, reciever.public_send(predicate, -1)
+        assert_same true, reciever.public_send(predicate, -2)
+        assert_same false, reciever.public_send(predicate, -3)
+        assert_same false, reciever.public_send(predicate, -(99 * 99))
+        assert_same true, reciever.public_send(predicate, 0.1)
+        assert_same true, reciever.public_send(predicate, 1.9)
+        assert_same false, reciever.public_send(predicate, '1')
+        assert_same false, reciever.public_send(predicate, :foo)
+        assert_same false, reciever.public_send(predicate, :bar)
+        assert_same false, reciever.public_send(predicate, :aliased)
+        assert_same false, reciever.public_send(predicate, :xyz)
+        assert_same false, reciever.public_send(predicate, BasicObject.new)
+        assert_same false, reciever.public_send(predicate, 'foo')
+        assert_same false, reciever.public_send(predicate, 'bar')
+        assert_same false, reciever.public_send(predicate, 'aliased')
+        assert_same false, reciever.public_send(predicate, 'xyz')
+      end
+    end
   end
 
   def test_has_key?
-    assert_same true, FooBar.has_key?(0)
-    assert_same true, FooBar.has_key?(1)
-    assert_same false, FooBar.has_key?(2)
-    assert_same false, FooBar.has_key?(99 * 99)
-    assert_same true, FooBar.has_key?(-1)
-    assert_same true, FooBar.has_key?(-2)
-    assert_same false, FooBar.has_key?(-3)
-    assert_same false, FooBar.has_key?(-(99 * 99))
-    assert_same true, FooBar.has_key?(0.1)
-    assert_same true, FooBar.has_key?(1.9)
-    assert_same false, FooBar.has_key?('1')
-    assert_same true, FooBar.has_key?(:foo)
-    assert_same true, FooBar.has_key?(:bar)
-    assert_same true, FooBar.has_key?(:aliased)
-    assert_same false, FooBar.has_key?(:xyz)
-    assert_same false, FooBar.has_key?(BasicObject.new)
-    assert_same true, FooBar.has_key?('foo')
-    assert_same true, FooBar.has_key?('bar')
-    assert_same true, FooBar.has_key?('aliased')
-    assert_same false, FooBar.has_key?('xyz')
+    assert_same true, Subclass.has_key?(0)
+    assert_same true, Subclass.has_key?(1)
+    assert_same false, Subclass.has_key?(2)
+    assert_same false, Subclass.has_key?(99 * 99)
+    assert_same true, Subclass.has_key?(-1)
+    assert_same true, Subclass.has_key?(-2)
+    assert_same false, Subclass.has_key?(-3)
+    assert_same false, Subclass.has_key?(-(99 * 99))
+    assert_same true, Subclass.has_key?(0.1)
+    assert_same true, Subclass.has_key?(1.9)
+    assert_same false, Subclass.has_key?('1')
+    assert_same true, Subclass.has_key?(:foo)
+    assert_same true, Subclass.has_key?(:bar)
+    assert_same true, Subclass.has_key?(:aliased)
+    assert_same false, Subclass.has_key?(:xyz)
+    assert_same false, Subclass.has_key?(BasicObject.new)
+    assert_same true, Subclass.has_key?('foo')
+    assert_same true, Subclass.has_key?('bar')
+    assert_same true, Subclass.has_key?('aliased')
+    assert_same false, Subclass.has_key?('xyz')
     
-    assert_same true, INS.has_key?(0)
-    assert_same true, INS.has_key?(1)
-    assert_same false, INS.has_key?(2)
-    assert_same false, INS.has_key?(99 * 99)
-    assert_same true, INS.has_key?(-1)
-    assert_same true, INS.has_key?(-2)
-    assert_same false, INS.has_key?(-3)
-    assert_same false, INS.has_key?(-(99 * 99))
-    assert_same true, INS.has_key?(0.1)
-    assert_same true, INS.has_key?(1.9)
-    assert_same false, INS.has_key?('1')
-    assert_same true, INS.has_key?(:foo)
-    assert_same true, INS.has_key?(:bar)
-    assert_same true, INS.has_key?(:aliased)
-    assert_same false, INS.has_key?(:xyz)
-    assert_same false, INS.has_key?(BasicObject.new)
-    assert_same true, INS.has_key?('foo')
-    assert_same true, INS.has_key?('bar')
-    assert_same true, INS.has_key?('aliased')
-    assert_same false, INS.has_key?('xyz')
+    assert_same true, INSTANCE.has_key?(0)
+    assert_same true, INSTANCE.has_key?(1)
+    assert_same false, INSTANCE.has_key?(2)
+    assert_same false, INSTANCE.has_key?(99 * 99)
+    assert_same true, INSTANCE.has_key?(-1)
+    assert_same true, INSTANCE.has_key?(-2)
+    assert_same false, INSTANCE.has_key?(-3)
+    assert_same false, INSTANCE.has_key?(-(99 * 99))
+    assert_same true, INSTANCE.has_key?(0.1)
+    assert_same true, INSTANCE.has_key?(1.9)
+    assert_same false, INSTANCE.has_key?('1')
+    assert_same true, INSTANCE.has_key?(:foo)
+    assert_same true, INSTANCE.has_key?(:bar)
+    assert_same true, INSTANCE.has_key?(:aliased)
+    assert_same false, INSTANCE.has_key?(:xyz)
+    assert_same false, INSTANCE.has_key?(BasicObject.new)
+    assert_same true, INSTANCE.has_key?('foo')
+    assert_same true, INSTANCE.has_key?('bar')
+    assert_same true, INSTANCE.has_key?('aliased')
+    assert_same false, INSTANCE.has_key?('xyz')
   end
 
   def test_with_aliases?
-    assert_same false, FooBar.with_aliases?(:foo)
-    assert_same true, FooBar.with_aliases?(:bar)
-    assert_same false, FooBar.with_aliases?(:aliased)
-    assert_same false, FooBar.with_aliases?(:xyz)
-    assert_same false, FooBar.with_aliases?(1)
-    assert_same false, FooBar.with_aliases?(BasicObject.new)
-    assert_same false, FooBar.with_aliases?('foo')
-    assert_same true, FooBar.with_aliases?('bar')
-    assert_same false, FooBar.with_aliases?('aliased')
-    assert_same false, FooBar.with_aliases?('xyz')
+    assert_same false, Subclass.with_aliases?(:foo)
+    assert_same true, Subclass.with_aliases?(:bar)
+    assert_same false, Subclass.with_aliases?(:aliased)
+    assert_same false, Subclass.with_aliases?(:xyz)
+    assert_same false, Subclass.with_aliases?(1)
+    assert_same false, Subclass.with_aliases?(BasicObject.new)
+    assert_same false, Subclass.with_aliases?('foo')
+    assert_same true, Subclass.with_aliases?('bar')
+    assert_same false, Subclass.with_aliases?('aliased')
+    assert_same false, Subclass.with_aliases?('xyz')
     
-    assert_same false, INS.with_aliases?(:foo)
-    assert_same true, INS.with_aliases?(:bar)
-    assert_same false, INS.with_aliases?(:aliased)
-    assert_same false, INS.with_aliases?(:xyz)
-    assert_same false, INS.with_aliases?(1)
-    assert_same false, INS.with_aliases?(BasicObject.new)
-    assert_same false, INS.with_aliases?('foo')
-    assert_same true, INS.with_aliases?('bar')
-    assert_same false, INS.with_aliases?('aliased')
-    assert_same false, INS.with_aliases?('xyz')
+    assert_same false, INSTANCE.with_aliases?(:foo)
+    assert_same true, INSTANCE.with_aliases?(:bar)
+    assert_same false, INSTANCE.with_aliases?(:aliased)
+    assert_same false, INSTANCE.with_aliases?(:xyz)
+    assert_same false, INSTANCE.with_aliases?(1)
+    assert_same false, INSTANCE.with_aliases?(BasicObject.new)
+    assert_same false, INSTANCE.with_aliases?('foo')
+    assert_same true, INSTANCE.with_aliases?('bar')
+    assert_same false, INSTANCE.with_aliases?('aliased')
+    assert_same false, INSTANCE.with_aliases?('xyz')
   end
 
 end
