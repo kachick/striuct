207eee39efe7197ba108cd8cbea30f16ad1ede0f
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-09-18 23:59:29 +0900

    improve test-case

--- test/test_subc-i-enum.rb
+++ test/test_subc-i-enum.rb
@@ -10,22 +10,19 @@ class Test_Striuct_Subclass_Instance_Enum < Test::Unit::TestCase
     member :address, /\A((\w+) ?)+\z/
     member :age, ->age{(20..140).include? age}
   end
-
-  def setup
-    @user = User.new 9999, 'taro', 'yamada', 'Tokyo Japan', 30
-    @user2 = User.new 9999, 'taro', 'yamada', 'Tokyo Japan', 30
-  end
   
   def test_each
-    assert_same @user, @user.each{}
-    assert_kind_of Enumerator, enum = @user.each
+    user = User.new 9999, 'taro', 'yamada', 'Tokyo Japan', 30
+    assert_same user, user.each{}
+    assert_kind_of Enumerator, enum = user.each
     assert_equal enum.next, 9999
     assert_equal enum.next, 'taro'
   end
   
   def test_each_member
-    assert_same @user, @user.each_member{}
-    assert_kind_of Enumerator, enum = @user.each_member
+    user = User.new 9999, 'taro', 'yamada', 'Tokyo Japan', 30
+    assert_same user, user.each_member{}
+    assert_kind_of Enumerator, enum = user.each_member
     assert_equal :id, enum.next
     assert_equal :last_name, enum.next
     assert_equal :family_name, enum.next
@@ -37,8 +34,9 @@ class Test_Striuct_Subclass_Instance_Enum < Test::Unit::TestCase
   end
   
   def test_each_index
-    assert_same @user, @user.each_index{}
-    assert_kind_of Enumerator, enum = @user.each_index
+    user = User.new 9999, 'taro', 'yamada', 'Tokyo Japan', 30
+    assert_same user, user.each_index{}
+    assert_kind_of Enumerator, enum = user.each_index
     assert_equal 0, enum.next
     assert_equal 1, enum.next
     assert_equal 2, enum.next
@@ -50,27 +48,29 @@ class Test_Striuct_Subclass_Instance_Enum < Test::Unit::TestCase
   end
   
   def test_each_with_index
-    assert_same @user, @user.each_with_index{}
-    assert_kind_of Enumerator, @user.each_with_index
+    user = User.new 9999, 'taro', 'yamada', 'Tokyo Japan', 30
+    assert_same user, user.each_with_index{}
+    assert_kind_of Enumerator, user.each_with_index
     
     r = []
-    @user.each_with_index do |value, index|
+    user.each_with_index do |value, index|
       r << [value, index]
     end
     
-    assert_equal [@user.each_value.to_a, @user.each_index.to_a].transpose, r
+    assert_equal [user.each_value.to_a, user.each_index.to_a].transpose, r
   end
 
   def test_each_member_with_index
-    assert_same @user, @user.each_member_with_index{}
-    assert_kind_of Enumerator, @user.each_member_with_index
+    user = User.new 9999, 'taro', 'yamada', 'Tokyo Japan', 30
+    assert_same user, user.each_member_with_index{}
+    assert_kind_of Enumerator, user.each_member_with_index
     
     r = []
-    @user.each_member_with_index do |name, index|
+    user.each_member_with_index do |name, index|
       r << [name, index]
     end
     
-    assert_equal [@user.each_member.to_a, @user.each_index.to_a].transpose, r
+    assert_equal [user.each_member.to_a, user.each_index.to_a].transpose, r
   end
   
   Sth = Striuct.define do
