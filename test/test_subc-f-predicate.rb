b1303f91878767140043b627a20e299516c8404b
Kenichi Kamiya <kachick1+github@gmail.com>
2012-09-18 11:38:51 +0900

    add predecate-test for class

--- test/test_subc-f-predicate.rb
+++ test/test_subc-f-predicate.rb
@@ -12,6 +12,17 @@ class Test_Striuct_Subclass_Predicate < Test::Unit::TestCase
   INS = FooBar.new
 
   def test_has_autonym?
+    assert_same true, FooBar.has_autonym?(:foo)
+    assert_same true, FooBar.has_autonym?(:bar)
+    assert_same false, FooBar.has_autonym?(:aliased)
+    assert_same false, FooBar.has_autonym?(:xyz)
+    assert_same false, FooBar.has_autonym?(1)
+    assert_same false, FooBar.has_autonym?(BasicObject.new)
+    assert_same true, FooBar.has_autonym?('foo')
+    assert_same true, FooBar.has_autonym?('bar')
+    assert_same false, FooBar.has_autonym?('aliased')
+    assert_same false, FooBar.has_autonym?('xyz')
+
     assert_same true, INS.has_autonym?(:foo)
     assert_same true, INS.has_autonym?(:bar)
     assert_same false, INS.has_autonym?(:aliased)
@@ -25,6 +36,17 @@ class Test_Striuct_Subclass_Predicate < Test::Unit::TestCase
   end
 
   def test_has_alias?
+    assert_same false, FooBar.has_alias?(:foo)
+    assert_same false, FooBar.has_alias?(:bar)
+    assert_same true, FooBar.has_alias?(:aliased)
+    assert_same false, FooBar.has_alias?(:xyz)
+    assert_same false, FooBar.has_alias?(1)
+    assert_same false, FooBar.has_alias?(BasicObject.new)
+    assert_same false, FooBar.has_alias?('foo')
+    assert_same false, FooBar.has_alias?('bar')
+    assert_same true, FooBar.has_alias?('aliased')
+    assert_same false, FooBar.has_alias?('xyz')
+
     assert_same false, INS.has_alias?(:foo)
     assert_same false, INS.has_alias?(:bar)
     assert_same true, INS.has_alias?(:aliased)
@@ -38,6 +60,17 @@ class Test_Striuct_Subclass_Predicate < Test::Unit::TestCase
   end
 
   def test_has_member?
+    assert_same true, FooBar.has_member?(:foo)
+    assert_same true, FooBar.has_member?(:bar)
+    assert_same true, FooBar.has_member?(:aliased)
+    assert_same false, FooBar.has_member?(:xyz)
+    assert_same false, FooBar.has_member?(1)
+    assert_same false, FooBar.has_member?(BasicObject.new)
+    assert_same true, FooBar.has_member?('foo')
+    assert_same true, FooBar.has_member?('bar')
+    assert_same true, FooBar.has_member?('aliased')
+    assert_same false, FooBar.has_member?('xyz')
+    
     assert_same true, INS.has_member?(:foo)
     assert_same true, INS.has_member?(:bar)
     assert_same true, INS.has_member?(:aliased)
@@ -51,6 +84,27 @@ class Test_Striuct_Subclass_Predicate < Test::Unit::TestCase
   end
 
   def test_has_index?
+    assert_same true, FooBar.has_index?(0)
+    assert_same true, FooBar.has_index?(1)
+    assert_same false, FooBar.has_index?(2)
+    assert_same false, FooBar.has_index?(99 * 99)
+    assert_same true, FooBar.has_index?(-1)
+    assert_same true, FooBar.has_index?(-2)
+    assert_same false, FooBar.has_index?(-3)
+    assert_same false, FooBar.has_index?(-(99 * 99))
+    assert_same true, FooBar.has_index?(0.1)
+    assert_same true, FooBar.has_index?(1.9)
+    assert_same false, FooBar.has_index?('1')
+    assert_same false, FooBar.has_index?(:foo)
+    assert_same false, FooBar.has_index?(:bar)
+    assert_same false, FooBar.has_index?(:aliased)
+    assert_same false, FooBar.has_index?(:xyz)
+    assert_same false, FooBar.has_index?(BasicObject.new)
+    assert_same false, FooBar.has_index?('foo')
+    assert_same false, FooBar.has_index?('bar')
+    assert_same false, FooBar.has_index?('aliased')
+    assert_same false, FooBar.has_index?('xyz')
+    
     assert_same true, INS.has_index?(0)
     assert_same true, INS.has_index?(1)
     assert_same false, INS.has_index?(2)
@@ -74,6 +128,27 @@ class Test_Striuct_Subclass_Predicate < Test::Unit::TestCase
   end
 
   def test_has_key?
+    assert_same true, FooBar.has_key?(0)
+    assert_same true, FooBar.has_key?(1)
+    assert_same false, FooBar.has_key?(2)
+    assert_same false, FooBar.has_key?(99 * 99)
+    assert_same true, FooBar.has_key?(-1)
+    assert_same true, FooBar.has_key?(-2)
+    assert_same false, FooBar.has_key?(-3)
+    assert_same false, FooBar.has_key?(-(99 * 99))
+    assert_same true, FooBar.has_key?(0.1)
+    assert_same true, FooBar.has_key?(1.9)
+    assert_same false, FooBar.has_key?('1')
+    assert_same true, FooBar.has_key?(:foo)
+    assert_same true, FooBar.has_key?(:bar)
+    assert_same true, FooBar.has_key?(:aliased)
+    assert_same false, FooBar.has_key?(:xyz)
+    assert_same false, FooBar.has_key?(BasicObject.new)
+    assert_same true, FooBar.has_key?('foo')
+    assert_same true, FooBar.has_key?('bar')
+    assert_same true, FooBar.has_key?('aliased')
+    assert_same false, FooBar.has_key?('xyz')
+    
     assert_same true, INS.has_key?(0)
     assert_same true, INS.has_key?(1)
     assert_same false, INS.has_key?(2)
@@ -97,6 +172,17 @@ class Test_Striuct_Subclass_Predicate < Test::Unit::TestCase
   end
 
   def test_with_aliases?
+    assert_same false, FooBar.with_aliases?(:foo)
+    assert_same true, FooBar.with_aliases?(:bar)
+    assert_same false, FooBar.with_aliases?(:aliased)
+    assert_same false, FooBar.with_aliases?(:xyz)
+    assert_same false, FooBar.with_aliases?(1)
+    assert_same false, FooBar.with_aliases?(BasicObject.new)
+    assert_same false, FooBar.with_aliases?('foo')
+    assert_same true, FooBar.with_aliases?('bar')
+    assert_same false, FooBar.with_aliases?('aliased')
+    assert_same false, FooBar.with_aliases?('xyz')
+    
     assert_same false, INS.with_aliases?(:foo)
     assert_same true, INS.with_aliases?(:bar)
     assert_same false, INS.with_aliases?(:aliased)
