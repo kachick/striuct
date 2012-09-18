6b5d7141f7d540bc53f7be4bbc04a3640a83459a
Kenichi Kamiya <kachick1@gmail.com>
2012-09-18 13:13:56 +0900

    improve test-case

--- test/test_subc-f-predicate.rb
+++ test/test_subc-f-predicate.rb
@@ -94,72 +94,48 @@ class Test_Striuct_Subclass_Predicate < Test::Unit::TestCase
     end
   end
 
-  def test_has_key?
-    assert_same true, Subclass.has_key?(0)
-    assert_same true, Subclass.has_key?(1)
-    assert_same false, Subclass.has_key?(2)
-    assert_same false, Subclass.has_key?(99 * 99)
-    assert_same true, Subclass.has_key?(-1)
-    assert_same true, Subclass.has_key?(-2)
-    assert_same false, Subclass.has_key?(-3)
-    assert_same false, Subclass.has_key?(-(99 * 99))
-    assert_same true, Subclass.has_key?(0.1)
-    assert_same true, Subclass.has_key?(1.9)
-    assert_same false, Subclass.has_key?('1')
-    assert_same true, Subclass.has_key?(:foo)
-    assert_same true, Subclass.has_key?(:bar)
-    assert_same true, Subclass.has_key?(:aliased)
-    assert_same false, Subclass.has_key?(:xyz)
-    assert_same false, Subclass.has_key?(BasicObject.new)
-    assert_same true, Subclass.has_key?('foo')
-    assert_same true, Subclass.has_key?('bar')
-    assert_same true, Subclass.has_key?('aliased')
-    assert_same false, Subclass.has_key?('xyz')
-    
-    assert_same true, INSTANCE.has_key?(0)
-    assert_same true, INSTANCE.has_key?(1)
-    assert_same false, INSTANCE.has_key?(2)
-    assert_same false, INSTANCE.has_key?(99 * 99)
-    assert_same true, INSTANCE.has_key?(-1)
-    assert_same true, INSTANCE.has_key?(-2)
-    assert_same false, INSTANCE.has_key?(-3)
-    assert_same false, INSTANCE.has_key?(-(99 * 99))
-    assert_same true, INSTANCE.has_key?(0.1)
-    assert_same true, INSTANCE.has_key?(1.9)
-    assert_same false, INSTANCE.has_key?('1')
-    assert_same true, INSTANCE.has_key?(:foo)
-    assert_same true, INSTANCE.has_key?(:bar)
-    assert_same true, INSTANCE.has_key?(:aliased)
-    assert_same false, INSTANCE.has_key?(:xyz)
-    assert_same false, INSTANCE.has_key?(BasicObject.new)
-    assert_same true, INSTANCE.has_key?('foo')
-    assert_same true, INSTANCE.has_key?('bar')
-    assert_same true, INSTANCE.has_key?('aliased')
-    assert_same false, INSTANCE.has_key?('xyz')
+  [:key?, :has_key?].each do |predicate|
+    TYPE_PAIRS.each_pair do |type, reciever|
+      define_method :"test_#{type}_#{predicate}" do
+        assert_same true, reciever.public_send(predicate, 0)
+        assert_same true, reciever.public_send(predicate, 1)
+        assert_same false, reciever.public_send(predicate, 2)
+        assert_same false, reciever.public_send(predicate, 99 * 99)
+        assert_same true, reciever.public_send(predicate, -1)
+        assert_same true, reciever.public_send(predicate, -2)
+        assert_same false, reciever.public_send(predicate, -3)
+        assert_same false, reciever.public_send(predicate, -(99 * 99))
+        assert_same true, reciever.public_send(predicate, 0.1)
+        assert_same true, reciever.public_send(predicate, 1.9)
+        assert_same false, reciever.public_send(predicate, '1')
+        assert_same true, reciever.public_send(predicate, :foo)
+        assert_same true, reciever.public_send(predicate, :bar)
+        assert_same true, reciever.public_send(predicate, :aliased)
+        assert_same false, reciever.public_send(predicate, :xyz)
+        assert_same false, reciever.public_send(predicate, BasicObject.new)
+        assert_same true, reciever.public_send(predicate, 'foo')
+        assert_same true, reciever.public_send(predicate, 'bar')
+        assert_same true, reciever.public_send(predicate, 'aliased')
+        assert_same false, reciever.public_send(predicate, 'xyz')
+      end
+    end
   end
 
-  def test_with_aliases?
-    assert_same false, Subclass.with_aliases?(:foo)
-    assert_same true, Subclass.with_aliases?(:bar)
-    assert_same false, Subclass.with_aliases?(:aliased)
-    assert_same false, Subclass.with_aliases?(:xyz)
-    assert_same false, Subclass.with_aliases?(1)
-    assert_same false, Subclass.with_aliases?(BasicObject.new)
-    assert_same false, Subclass.with_aliases?('foo')
-    assert_same true, Subclass.with_aliases?('bar')
-    assert_same false, Subclass.with_aliases?('aliased')
-    assert_same false, Subclass.with_aliases?('xyz')
-    
-    assert_same false, INSTANCE.with_aliases?(:foo)
-    assert_same true, INSTANCE.with_aliases?(:bar)
-    assert_same false, INSTANCE.with_aliases?(:aliased)
-    assert_same false, INSTANCE.with_aliases?(:xyz)
-    assert_same false, INSTANCE.with_aliases?(1)
-    assert_same false, INSTANCE.with_aliases?(BasicObject.new)
-    assert_same false, INSTANCE.with_aliases?('foo')
-    assert_same true, INSTANCE.with_aliases?('bar')
-    assert_same false, INSTANCE.with_aliases?('aliased')
-    assert_same false, INSTANCE.with_aliases?('xyz')
+  [:with_aliases?].each do |predicate|
+    TYPE_PAIRS.each_pair do |type, reciever|
+      define_method :"test_#{type}_#{predicate}" do
+        assert_same false, reciever.public_send(predicate, :foo)
+        assert_same true, reciever.public_send(predicate, :bar)
+        assert_same false, reciever.public_send(predicate, :aliased)
+        assert_same false, reciever.public_send(predicate, :xyz)
+        assert_same false, reciever.public_send(predicate, 1)
+        assert_same false, reciever.public_send(predicate, BasicObject.new)
+        assert_same false, reciever.public_send(predicate, 'foo')
+        assert_same true, reciever.public_send(predicate, 'bar')
+        assert_same false, reciever.public_send(predicate, 'aliased')
+        assert_same false, reciever.public_send(predicate, 'xyz')
+      end
+    end
   end
 
 end
