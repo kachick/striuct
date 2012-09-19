9d2e0bf4787a1f44f699a20fc3e0cba4b89200c1
Kenichi Kamiya <kachick1@gmail.com>
2012-09-20 05:03:21 +0900

    improve method place

--- lib/striuct/instancemethods/default.rb
+++ lib/striuct/instancemethods/default.rb
@@ -9,6 +9,28 @@ class Striuct; module InstanceMethods
     default_value_for(autonym) == fetch_for_autonym(autonym)
   end
 
+  private
+
+  # @param [Symbol] target_autonyms - MUST already converted to native autonym
+  # @return [self]
+  def _set_defaults(*target_autonyms)
+    target_autonyms.each do |autonym|
+      if with_default? autonym
+        default = default_value_for autonym
+        _set autonym, (
+          if default_type_for(autonym) == :lazy
+            args = [self, autonym][0, default.arity]
+            default.call(*args)
+          else
+            default
+          end
+        )
+      end
+    end
+    
+    self
+  end
+
   # @endgroup
 
 end; end
--- lib/striuct/instancemethods/object.rb
+++ lib/striuct/instancemethods/object.rb
@@ -6,13 +6,12 @@ class Striuct; module InstanceMethods
     @db, @locks = {}, {}
     replace_values(*values)
     excess_autonyms = _autonyms.last(size - values.size)
-    set_defaults(*excess_autonyms)
+    _set_defaults(*excess_autonyms)
   end
 
   # @return [self]
   def freeze
-    @db.freeze
-    @locks.freeze
+    @db.freeze; @locks.freeze
     super
   end
   
@@ -20,30 +19,6 @@ class Striuct; module InstanceMethods
   
   def initialize_copy(original)
     @db, @locks = @db.dup, {}
-  end
-
-  # @endgroup
-
-  # @group Default Value
-
-  # @param [Symbol] target_autonyms - MUST already converted to native autonym
-  # @return [self]
-  def set_defaults(*target_autonyms)
-    target_autonyms.each do |autonym|
-      if with_default? autonym
-        default = default_value_for autonym
-        _set autonym, (
-          if default_type_for(autonym) == :lazy
-            args = [self, autonym][0, default.arity]
-            default.call(*args)
-          else
-            default
-          end
-        )
-      end
-    end
-    
-    self
   end
 
   # @endgroup
