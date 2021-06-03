1ba9516e4b384b9a808f6914d8939b08f40e2c0e
Kenichi Kamiya <kachick1@gmail.com>
2021-06-03 23:27:16 +0900

    Improve test/helper.rb

--- test/helper.rb
+++ test/helper.rb
@@ -1,4 +1,22 @@
-# frozen_string_literal: false
+# coding: us-ascii
+# frozen_string_literal: true
+
+require 'warning'
+
+# How to use => https://test-unit.github.io/test-unit/en/
 require 'test/unit'
+
+require 'irb'
+require 'power_assert/colorize'
+require 'irb/power_assert'
+
+if Warning.respond_to?(:[]=) # @TODO Removable this guard after dropped ruby 2.6
+  Warning[:deprecated] = true
+  Warning[:experimental] = true
+end
+
+Warning.process do |_warning|
+  :raise
+end
 
 require_relative '../lib/striuct'
