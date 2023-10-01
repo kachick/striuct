c47001a0b2e030b93790920f1a8ea0c070447774
Kenichi Kamiya <kachick1@gmail.com>
2023-10-01 19:50:09 +0900

    Use Nix flakes

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  inputs = {
+    # Candidate channels
+    #   - https://github.com/kachick/anylang-template/issues/17
+    #   - https://discourse.nixos.org/t/differences-between-nix-channels/13998
+    # How to update the revision
+    #   - `nix flake update --commit-lock-file` # https://nixos.org/manual/nix/stable/command-ref/new-cli/nix3-flake-update.html
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = with pkgs;
+          mkShell {
+            buildInputs = [
+              # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
+              # https://github.com/kachick/dotfiles/pull/228
+              bashInteractive
+
+              ruby_3_2
+              # Required to build psych via irb dependency
+              # https://github.com/kachick/irb-power_assert/issues/116
+              # https://github.com/ruby/irb/pull/648
+              libyaml
+
+              dprint
+              tree
+              nil
+              nixpkgs-fmt
+              typos
+            ];
+          };
+      });
+}
